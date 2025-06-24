0415f5aa4038582e2bfba42f16128264df726411
Trung-Le914 <trung149hn@gmail.com>
2025-06-24 22:52:22 +0700

    Add files via upload

---
+++ Trigo_Approx.py
@@ -0,0 +1,16 @@
+def approx_sinh(x, n):
+    """
+    Approximate the hyperbolic sine of x using the Taylor series expansion.
+    Parameters:
+    x (float): The input value.
+    n (int): Number of terms in the Taylor series expansion.
+    Returns:
+    float: Approximate value of sinh(x) using n+1 terms.
+    """
+    result = x
+    
+    for i in range(1,n+1):
+        result += (x**(2*i+1))/(factorial(2*i+1))
+    
+    return result
+    
